f172832d86a429e6adbf02620cfd53f1ade946d4
Sheldon Lai <sheldon.lai@egistec.com>
2016-04-06 15:57:03 +0800

    refactor code

--- spelltest.c
+++ spelltest.c
@@ -9,21 +9,22 @@
 #define MAX_TOKEN_SIZE 31
 #define MAX_CORRECTION 5
 
-static void build_hashmap(FILE *fp_corpus, CMap *map);
-static int edit_distance(const char *s, int len_s, const char *t, int len_t);
-static void find_correct_spelling(const int count, CELL_INFO *info,
-	const char *key, void *a, void *b);
-
 typedef struct {
 	const char *key;
 	int edit_distance;
 	int frequency;
 } CELL_INFO;
 
-CVector *g_cvector;
+CVector *g_leader_board;
 CELL_INFO g_ed_info;
 _Bool g_is_stop = false;
 static int g_current_count = 0;
+
+/* Function Prototypes */
+static void build_hashmap(FILE *fp_corpus, CMap *map);
+static int edit_distance(const char *s, int len_s, const char *t, int len_t);
+static void update_leader_board(const int count, CELL_INFO *info,
+	const char *key, void *a, void *b);
 
 void vectorMapFn(void *elemAddr, void *auxData)
 {
@@ -43,7 +44,7 @@ int compareFn(const void *elemAddr1, const void *elemAddr2)
 	return strcmp(elemAddr1, ((CELL_INFO*)elemAddr2)->key);
 }
 
-static void find_correct_spelling(const int count, CELL_INFO *info,
+static void update_leader_board(const int count, CELL_INFO *info,
 	const char *key, void *a, void *b)
 {
 	int ed = edit_distance(key, strlen(key), b, strlen(b));
@@ -53,41 +54,41 @@ static void find_correct_spelling(const int count, CELL_INFO *info,
 	//printf("%-30s %d \tedit_distance=%d\n", key, *(int*)a, ed);
 	if(count < MAX_CORRECTION)
 	{
-		int found = CVectorSearch(g_cvector, key, compareFn, 0, false);
-		if(found == -1) CVectorAppend(g_cvector, &g_ed_info);
+		int found = CVectorSearch(g_leader_board, key, compareFn, 0, false);
+		if(found == -1) CVectorAppend(g_leader_board, &g_ed_info);
 	}
 	else
 	{
 		for(int i = 0 ; i < count ; i++)
 		{
-		  info = (CELL_INFO*)CVectorNth(g_cvector, i);
+		  info = (CELL_INFO*)CVectorNth(g_leader_board, i);
 			if(ed < info->edit_distance)
 			{
-			  CVectorReplace(g_cvector, &g_ed_info, i);
+			  CVectorReplace(g_leader_board, &g_ed_info, i);
 			  break;
 		  }
 			else if(ed == info->edit_distance && g_ed_info.frequency > info->frequency)
 			{
-			  CVectorReplace(g_cvector, &g_ed_info, i);
+			  CVectorReplace(g_leader_board, &g_ed_info, i);
 			  break;
 			}
 		}
 	}
 }
 
-void printMap(const char *key, void *a, void *b)
+void find_possible_spelling(const char *key, void *a, void *b)
 {
 	// prune the work, find the 5 best correction ed <=3 then skip the rest
 	if(g_is_stop) return;
 	g_current_count++;
 	CELL_INFO *info;
-	int count = CVectorCount(g_cvector);
+	int count = CVectorCount(g_leader_board);
 	if(count == MAX_CORRECTION)
 	{
 		int well_choice = 0;
 		for(int i = 0 ; i < count ; i++)
 		{
-		  info = (CELL_INFO*)CVectorNth(g_cvector, i);
+		  info = (CELL_INFO*)CVectorNth(g_leader_board, i);
 			if(info->edit_distance <= 3) well_choice++;
 		}
 		if(well_choice == count && g_current_count > 200)
@@ -96,12 +97,12 @@ void printMap(const char *key, void *a, void *b)
 		   return;
 	  }
 	}
-	find_correct_spelling(count, info, key, a, b);
+	update_leader_board(count, info, key, a, b);
 }
 
-void printMap2(const char *key, void *a, void *b)
+void find_misspelling(const char *key, void *a, void *b)
 {
-	FILE *fp = (FILE*)b;
+	CMap *map_corpus = (CMap*)b;
 }
 
 static void build_hashmap(FILE *fp_corpus, CMap *map)
@@ -158,41 +159,50 @@ int main(int argc, char *argv[])
     fprintf(stderr, "you have to input two arguments\n");
     return -1;
   }
-  FILE *fp_corpus, *fp_spellcheck = NULL;
+  FILE *fp_corpus, *fp_document = NULL;
   char *word_check;
   fp_corpus = fopen(argv[1], "r");
   assert(fp_corpus != NULL);
   if(strchr(argv[2], '.') != NULL)
-    fp_spellcheck = fopen(argv[2], "r");
+    fp_document = fopen(argv[2], "r");
   else
     word_check = argv[2];
-	CMap *map = CMapCreate(sizeof(int), 1000, NULL);
-	g_cvector = CVectorCreate(sizeof(CELL_INFO), MAX_CORRECTION, NULL);
+	CMap *map_corpus = CMapCreate(sizeof(int), 1000, NULL);
+	g_leader_board = CVectorCreate(sizeof(CELL_INFO), MAX_CORRECTION, NULL);
 	//build model
 	printf("build the model...\n");
-  build_hashmap(fp_corpus, map);
+  build_hashmap(fp_corpus, map_corpus);
 	printf("find the best 5 corrections...\n");
-	if(fp_spellcheck == NULL)
+	if(fp_document == NULL)
 	{
-		int *val = (int *)CMapGet(map, word_check);
+		int *val = (int *)CMapGet(map_corpus, word_check);
 		if(val == NULL)
-  	  CMapMap(map, printMap, word_check);
-		else
+		{
+  	  CMapMap(map_corpus, find_possible_spelling, word_check);
+			CVectorSort(g_leader_board, vecCompareFn);
+			//print out the answer
+			printf("%s :", word_check);
+			int count = CVectorCount(g_leader_board);
+			CELL_INFO *info;
+			for(int i = 0 ; i < count ; i++){
+				info = (CELL_INFO*)CVectorNth(g_leader_board, i);
+				printf("%s ", info->key);
+			}
+			printf("\n");
+			g_is_stop = false;
+			g_current_count = 0;
+		}else
 			printf("the word %s is correct\n", word_check);
-	}else{
-  	  CMapMap(map, printMap2, fp_spellcheck);
 	}
-	CVectorSort(g_cvector, vecCompareFn);
-	//CVectorMap(g_cvector, vectorMapFn, NULL);
-	printf("%s :", word_check);
-	int count = CVectorCount(g_cvector);
-	CELL_INFO *info;
-	for(int i = 0 ; i < count ; i++){
-		info = (CELL_INFO*)CVectorNth(g_cvector, i);
-		printf("%s ", info->key);
+	else
+	{
+		CMap *map_document = CMapCreate(sizeof(int), 1000, NULL);
+		build_hashmap(fp_document, map_document);
+  	CMapMap(map_document, find_misspelling, map_corpus);
+		CMapDispose(map_document);
 	}
-	printf("\n");
-	g_is_stop = false;
-	g_current_count = 0;
+	//CVectorMap(g_leader_board, vectorMapFn, NULL);
+	CVectorDispose(g_leader_board);
+	CMapDispose(map_corpus);
   return 0;
 }
